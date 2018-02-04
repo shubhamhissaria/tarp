65ff8c5a5d8500cd6404ea9a01ed97af5e9e6d09
Naman Bhaia <namanbhaia@gmail.com>
2018-02-05 02:21:58 +0530

    Login page error
    Not reading from input box

---
+++ TARP/app/src/main/java/com/example/naman/tarp/LoginActivity.java
@@ -0,0 +1,110 @@
+package com.example.naman.tarp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public EditText inputregno, inputPassword;
+    public ProgressBar progressBar;
+    public Button btnStudSignup, btnFacSignup, btnLogin, btnReset;
+
+    DatabaseReference data;
+    String user, passw,regno,password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        inputregno = (EditText) findViewById(R.id.regno);
+        inputPassword = (EditText) findViewById(R.id.password);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        btnStudSignup = (Button) findViewById(R.id.btn_studsignup);
+        btnFacSignup = (Button) findViewById(R.id.btn_facsignup);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnReset = (Button) findViewById(R.id.btn_reset_password);
+
+        regno = inputregno.getText().toString().trim();
+        password = inputPassword.getText().toString().trim();
+
+        data = FirebaseDatabase.getInstance().getReference().child(regno);
+        //Toast.makeText(getApplicationContext(), password, Toast.LENGTH_SHORT).show();
+        //Toast.makeText(getApplicationContext(), data.child("name").toString(), Toast.LENGTH_SHORT).show();
+
+        btnStudSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, StudentSignUp.class));
+            }
+        });
+        btnFacSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, FacultySignUp.class));
+            }
+        });
+
+//        btnReset.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(LoginActivity.this, ResetPasswordActivity.class));
+//            }
+//        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(regno)) {
+                    Toast.makeText(getApplicationContext(), "Enter registration number!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+                data.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        user = dataSnapshot.child("regno").getValue(String.class);
+                        //Toast.makeText(getApplicationContext(), user, Toast.LENGTH_SHORT).show();
+                        passw = dataSnapshot.child("pass").getValue(String.class);
+                        if(regno.equals(user) && password.equals(passw))
+                        {
+//                            Intent intent = new Intent(LoginActivity.this, DisplayHome.class);
+//                            intent.putExtra("accno", accnumber);
+//                            startActivity(intent);
+//                            finish();
+                            Toast.makeText(LoginActivity.this, getString(R.string.auth_success), Toast.LENGTH_LONG).show();
+                        }
+                        else
+                            Toast.makeText(LoginActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Toast.makeText(LoginActivity.this, getString(R.string.database_error), Toast.LENGTH_LONG).show();
+                    }
+                });
+//
+//
+            }
+        });
+    }
+}
