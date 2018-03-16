81b72033c42c4cfcd550a88d5bd14510b3a61a94
chirag-mehta <mchirag822@gmail.com>
2018-03-16 15:29:23 +0530

    retival01 contains code to retrive any data from database

---
+++ retrival01/app/src/main/java/com/example/chiragmehta/retrival01/Schedule.java
@@ -0,0 +1,24 @@
+package com.example.chiragmehta.retrival01;
+
+/**
+ * Created by Chirag Mehta on 16/03/2018.
+ */
+
+public class Schedule {
+    private String regno;
+
+    public Schedule() {
+
+    }
+    public Schedule(String regno) {
+        this.regno = regno;
+    }
+
+    public String getRegno() {
+        return regno;
+    }
+
+    public void setRegno(String regno) {
+        this.regno = regno;
+    }
+}
